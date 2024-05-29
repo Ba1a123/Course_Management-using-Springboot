8b6e7f72d13ae76f455d3dc98678a03b2c0dd415
Bala <balapeesala1022@gmail.com>
2024-05-29 19:46:04 +0530

    Initializing the project

---
+++ src/main/java/com/example/course_management/CourseManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.course_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementApplication.class, args);
+	System.out.println("Hi i am Bala");
+	}
+
+}
